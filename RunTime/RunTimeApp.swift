7d0b6df0f84f7b5fa2df5bffe8536b9fc2f29e34
Tommy O'Donnell <s015862@students.lmsd.org>
2022-01-06 14:20:52 -0500

    Initial Commit

---
+++ RunTime/RunTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTimeApp.swift
+//  RunTime
+//
+//  Created by Thomas O'Donnell (student LM) on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct RunTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

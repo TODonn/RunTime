ba016f00c594a68586ebff5453c2bfe7cb2755d2
Elson Lin <s033046@students.lmsd.org>
2022-01-11 13:09:47 -0500

    imported api

--- RunTime/Food.swift
+++ RunTime/Food.swift
@@ -9,4 +9,27 @@ import Foundation
 
 struct Food {
     
+    let headers = [
+        "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com",
+        "x-rapidapi-key": "09efb9c89amshf6806ede7a42469p13a7e0jsnd50f17d892ae"
+    ]
+    
+    let request = NSMutableURLRequest(url: NSURL(string: "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple")! as URL,
+        cachePolicy: .useProtocolCachePolicy,
+            timeoutInterval: 10.0)
+    request.httpMethod = "GET"
+    request.allHTTPHeaderFields = headers
+    
+    let session = URLSession.shared
+    let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+        if (error != nil) {
+            print(error)
+        } else {
+            let httpResponse = response as? HTTPURLResponse
+            print(httpResponse)
+        }
+    })
+    
+    dataTask.resume()
+    
 }

1ae30882326fdbfd368eba3e576511e4b295b3a0
Andy Sun <sunandy07@gmail.com>
2022-01-11 11:14:36 -0500

    Added a detailed view, didn't do anything with it

---
+++ RunTime/DetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedView.swift
+//  RunTime
+//
+//  Created by Andy Sun (student LM) on 1/10/22.
+//
+
+import SwiftUI
+
+struct DetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedView()
+    }
+}

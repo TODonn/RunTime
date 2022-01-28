f48f18a1183bda3422b7dee6ee688ea4eb9c7239
elsonl <95712877+elsonl@users.noreply.github.com>
2022-01-28 09:38:36 -0500

    Update ContentView.swift

--- RunTime/ContentView.swift
+++ RunTime/ContentView.swift
@@ -9,40 +9,36 @@ import SwiftUI
 
 
 struct ContentView: View {
-    //   @StateObject var foodItem = Food()
     
     @StateObject var food : Food
-    @State var name : String
+    @State var name : String = "" // Keeps track of name
+    
     
     
     var body: some View{
         
         NavigationView{
             ZStack{
-                Color.blue
+                Color.blue //sets backgroun color
                 VStack{
                     Text("NutriFacts")
                         .foregroundColor(.white)
                         .font(.system(size: 44))
-                    
-                        
-                    Text("Enter a Food Below").bold().foregroundColor(.black).font(.system(size: 18))
+                    Text("Enter Food Below").bold().foregroundColor(.black).font(.system(size: 24))
                     VStack{
                         TextField("Search ...", text: $name).foregroundColor(Color.black).background(Color(.systemGray4))
-                            
-                
+                        //TextField that takes in user input for the name
                     }
                     
-                    NavigationLink(destination: DetailedView(name: $name, food: Food(name: Binding.constant("\(name)")), count: 0)) {
-                        Text("Search")
-                            .foregroundColor(.white)
-                            .font(.system(size: 22))
+                    NavigationLink(destination: DetailedView(name: Binding.constant("\(name)"), food: Food(name: Binding.constant("\(name)")))) {
+                        Text("Search") .foregroundColor(Color.white)
+                        
+                        //navigation link that takes user to detailed view once search is pushed
                     }
-                
                 }
                 
             }.ignoresSafeArea()
-        }
+        }.navigationViewStyle(StackNavigationViewStyle())
         
         
         
@@ -52,7 +48,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView(food: Food(name: Binding.constant("pineapple")), name: " ")
+            ContentView(food: Food(name: Binding.constant("pineapple")))
         }
     }
 }

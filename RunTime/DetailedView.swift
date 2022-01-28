dea911166f644b40af86339b984a755fc4c06c07
elsonl <95712877+elsonl@users.noreply.github.com>
2022-01-28 09:39:07 -0500

    Update DetailedView.swift

--- RunTime/DetailedView.swift
+++ RunTime/DetailedView.swift
@@ -1,9 +1,6 @@
 
 // SwiftUI File: Detailed View
-// Written by: Andy Sun
-//Date: 1/11/22 - 1/xx/22
-//Description: The view when the dropdown is pressed. Contains the image, name of the food, and nutrients.
-
+// Written by: Elson Lin
 
 
 
@@ -15,28 +12,33 @@ struct DetailedView: View {
     
     @Binding var name : String
     @ObservedObject var food : Food
-    @State var count : Int
+    @State var count : Int = 0
     
     var body: some View {
         
-        
-        
-        List(food.responses.hints.indices){ count in
-            
-            
-            NavigationLink( destination:    DetailedViewDetail(food: Food(name: Binding.constant(food.responses.hints[count].food.label ?? "nil")), name: Binding.constant((food.responses.hints.first?.food.label ?? "nil")), count: Binding.constant(0)),
+        if (food.responses.hints.first?.food.label == nil){
+            KFImage(URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRZ8QFc0iANE-OQiuVbsiwfbkL6JbE6Wqn6g&usqp=CAU:")).resizable().aspectRatio(contentMode: .fit)
+            Text("Check your Spelling!")
+        }
+        //Displays error image if name is nil
+        List(food.responses.hints.indices){ counts in
+            // prints a list of all resultss
+            NavigationLink( destination:    DetailedViewDetail(food: Food(name: Binding.constant(food.responses.hints.first?.food.label ?? "nil")), name: Binding.constant((food.responses.hints[counts].food.label ?? "nil")), count: Binding.constant(counts)),
                             label: {
-                Text(verbatim: "\(food.responses.hints[count].food.label) \(count)".replacingOccurrences(of: "Optional(\"", with: "").replacingOccurrences(of: "\")", with: ""))
-                
-                
-            })
+                                Text(verbatim: "\((counts)+1). \(food.responses.hints[counts].food.label) "
+                                        //numbers each listing and prints out each food item
+                                        .replacingOccurrences(of: "Optional(\"", with: "").replacingOccurrences(of: "\")", with: ""))
+                                
+                                
+                            })
             
-        } .navigationBarTitle("JSON DATA")
+        } .navigationBarTitle("\(name)") .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
+
 struct DetailedView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailedView(name: .constant("Pineapple"), food: Food(name: Binding.constant("Pineapple")), count: 0)
+        DetailedView(name: .constant("Pineapple"), food: Food(name: Binding.constant("Pineapple")))
     }
 }
